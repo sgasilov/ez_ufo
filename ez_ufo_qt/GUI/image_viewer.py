e32ab118ff5c582ef4cb3634c83700202a60aa14
emsliei <iainemslie@gmail.com>
2021-06-29 21:02:36 -0600

    Added set scroller to midpoint upon image stack loaded

--- ez_ufo_qt/GUI/image_viewer.py
+++ ez_ufo_qt/GUI/image_viewer.py
@@ -141,9 +141,10 @@ class ImageViewerGroup(QGroupBox):
                 self.tiff_arr = image_read_write.read_all_images(dir, tiff_list)
                 self.scroller.setRange(0, self.tiff_arr.shape[0] - 1)
                 self.scroller.setEnabled(True)
-                print(self.tiff_arr.shape[0])
                 self.image_window.setImage(self.tiff_arr[0])
                 msg.close()
+                mid_index = self.tiff_arr.shape[0] / 2
+                self.scroller.setValue(mid_index)
             except image_read_write.InvalidDataSetError:
                 print("Invalid Data Set")
 
@@ -161,6 +162,8 @@ class ImageViewerGroup(QGroupBox):
             self.scroller.setEnabled(True)
             self.image_window.setImage(self.tiff_arr[0])
             msg.close()
+            mid_index = self.tiff_arr.shape[0] / 2
+            self.scroller.setValue(mid_index)
         except image_read_write.InvalidDataSetError:
             print("Invalid Data Set")
 
