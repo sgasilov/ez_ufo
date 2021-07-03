59b0a4f3d98e8fe3c02eaf04044e7e76400afae5
emsliei <iainemslie@gmail.com>
2021-07-02 21:21:07 -0600

    Fixed fix-nan

--- ez_ufo_qt/main.py
+++ ez_ufo_qt/main.py
@@ -35,10 +35,10 @@ def get_CTdirs_list(inpath, dirtype):
 def frmt_ufo_cmds(cmds, ctset, out_pattern, ax, args, Tofu, Ufo, FindCOR, nviews, WH):
     '''formats list of processing commands for a CT set'''
     # two helper variables to mark that PR/FFC has been done at some step
-    if args.sinFFC is False:
-        swiFFC = True
-    else:
+    if args.sinFFC is True:
         swiFFC = False
+    else:
+        swiFFC = True
     swiPR = args.PR  # PR is an optional operation
 
     ####### PREPROCESSING #########
