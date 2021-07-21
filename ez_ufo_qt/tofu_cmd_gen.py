8b7db44a3771f214c0bcec915be4a70414d3106a
emsliei <iainemslie@gmail.com>
2021-07-20 18:08:55 -0600

    Changed logic for phase retrieval

--- ez_ufo_qt/tofu_cmd_gen.py
+++ ez_ufo_qt/tofu_cmd_gen.py
@@ -164,34 +164,40 @@ class tofu_cmds(object):
         cmd += ' --output {}'.format(os.path.dirname(out_pattern))
         return cmd
 
-    def get_pr_tofu_cmd(self, ctset, args, nviews, WH):
-        #indir will format paths to flats dakrs and tomo2 correctly even if they were
-        #pre-processed, however path to the input directory with projections
+    def get_pr_tofu_cmd_sinFFC(self, ctset, args, nviews, WH):
+        # indir will format paths to flats darks and tomo2 correctly even if they were
+        # pre-processed, however path to the input directory with projections
         # cannot be formatted with that command correctly
         indir = self.make_inpaths(ctset[0], ctset[1])
         # so we need a separate "universal" command which considers all previous steps
-        in_proj_dir, out_pattern = fmt_in_out_path(args.tmpdir,ctset[0], self._fdt_names[2])
-        if args.sinFFC:
-            # Phase retrieval
-            cmd = 'tofu preprocess --delta 1e-6'
-            cmd += ' --energy {} --propagation-distance {}' \
-                   ' --pixel-size {} --regularization-rate {:0.2f}' \
-                .format(args.energy, args.z, args.pixel, args.log10db)
-            #cmd += ' --retrieval-padded-width {}'.format(WH[0])
-            #cmd += ' --retrieval-padded-height {}'.format(WH[1])
-            cmd += ' --projections {}'.format(in_proj_dir)
-            cmd += ' --output {}'.format(out_pattern)
-            return cmd
-        elif not args.sinFFC:
-            cmd = 'tofu preprocess --fix-nan-and-inf --projection-filter none --delta 1e-6'
-            cmd += ' --darks {} --flats {} --projections {}'.format(indir[0],indir[1],in_proj_dir)
-            if ctset[1]==4:
-                cmd += ' --flats2 {}'.format(indir[3])
-            cmd += ' --output {}'.format(out_pattern)
-            cmd += ' --energy {} --propagation-distance {}'\
-                   ' --pixel-size {} --regularization-rate {:0.2f}'\
-                   .format(args.energy, args.z, args.pixel, args.log10db)
-            return cmd
+        in_proj_dir, out_pattern = fmt_in_out_path(args.tmpdir, ctset[0], self._fdt_names[2])
+        # Phase retrieval
+        cmd = 'tofu preprocess --delta 1e-6'
+        cmd += ' --energy {} --propagation-distance {}' \
+               ' --pixel-size {} --regularization-rate {:0.2f}' \
+            .format(args.energy, args.z, args.pixel, args.log10db)
+        # cmd += ' --retrieval-padded-width {}'.format(WH[0])
+        # cmd += ' --retrieval-padded-height {}'.format(WH[1])
+        cmd += ' --projections {}'.format(in_proj_dir)
+        cmd += ' --output {}'.format(out_pattern)
+        return cmd
+
+    def get_pr_tofu_cmd(self, ctset, args, nviews, WH):
+        # indir will format paths to flats darks and tomo2 correctly even if they were
+        # pre-processed, however path to the input directory with projections
+        # cannot be formatted with that command correctly
+        indir = self.make_inpaths(ctset[0], ctset[1])
+        # so we need a separate "universal" command which considers all previous steps
+        in_proj_dir, out_pattern = fmt_in_out_path(args.tmpdir, ctset[0], self._fdt_names[2])
+        cmd = 'tofu preprocess --fix-nan-and-inf --projection-filter none --delta 1e-6'
+        cmd += ' --darks {} --flats {} --projections {}'.format(indir[0], indir[1], in_proj_dir)
+        if ctset[1] == 4:
+            cmd += ' --flats2 {}'.format(indir[3])
+        cmd += ' --output {}'.format(out_pattern)
+        cmd += ' --energy {} --propagation-distance {}' \
+               ' --pixel-size {} --regularization-rate {:0.2f}' \
+            .format(args.energy, args.z, args.pixel, args.log10db)
+        return cmd
 
     def get_reco_cmd(self, ctset, out_pattern, ax, args, nviews, WH, ffc, PR):
         #direct CT reconstruction from input dir to output dir;
