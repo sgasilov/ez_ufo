7e3b795a612eac3ab9b0539e22b2ccc1a1243d31
emsliei <iainemslie@gmail.com>
2021-07-30 18:10:14 -0600

    Added GUI template for method options to FFC

--- ez_ufo_qt/GUI/ffc.py
+++ ez_ufo_qt/GUI/ffc.py
@@ -1,5 +1,5 @@
 import logging
-from PyQt5.QtWidgets import QGridLayout, QLabel, QGroupBox, QLineEdit, QCheckBox
+from PyQt5.QtWidgets import QGridLayout, QLabel, QGroupBox, QLineEdit, QCheckBox, QRadioButton, QHBoxLayout
 
 import ez_ufo_qt.GUI.params as parameters
 
@@ -13,22 +13,26 @@ class FFCGroup(QGroupBox):
         self.setTitle("Flat Field Correction")
         self.setStyleSheet('QGroupBox {color: indigo;}')
 
-        self.enable_sinFFC_checkbox = QCheckBox()
-        self.enable_sinFFC_checkbox.setText("Use Smart Intensity Normalization Flat Field Correction")
+        self.method_label = QLabel("Method:")
+
+        self.average_rButton = QRadioButton("Average")
+
+        self.ssim_rButton = QRadioButton("SSIM")
+
+        self.eigen_rButton = QRadioButton("Eigen")
+
+        self.enable_sinFFC_checkbox = QCheckBox("Use Smart Intensity Normalization Flat Field Correction")
         self.enable_sinFFC_checkbox.stateChanged.connect(self.set_sinFFC)
 
-        self.eigen_pco_repetitions_label = QLabel()
-        self.eigen_pco_repetitions_label.setText("Eigen PCO Repetitions")
+        self.eigen_pco_repetitions_label = QLabel("Eigen PCO Repetitions")
         self.eigen_pco_repetitions_entry = QLineEdit()
         self.eigen_pco_repetitions_entry.textChanged.connect(self.set_pcoReps)
 
-        self.eigen_pco_downsample_label = QLabel()
-        self.eigen_pco_downsample_label.setText("Eigen PCO Downsample")
+        self.eigen_pco_downsample_label = QLabel("Eigen PCO Downsample")
         self.eigen_pco_downsample_entry = QLineEdit()
         self.eigen_pco_downsample_entry.textChanged.connect(self.set_pcoDowns)
 
-        self.downsample_label = QLabel()
-        self.downsample_label.setText("Downsample")
+        self.downsample_label = QLabel("Downsample")
         self.downsample_entry = QLineEdit()
         self.downsample_entry.textChanged.connect(self.set_downsample)
 
@@ -37,13 +41,20 @@ class FFCGroup(QGroupBox):
     def set_layout(self):
         layout = QGridLayout()
 
+        rbutton_layout = QHBoxLayout()
+        rbutton_layout.addWidget(self.method_label)
+        rbutton_layout.addWidget(self.average_rButton)
+        rbutton_layout.addWidget(self.ssim_rButton)
+        rbutton_layout.addWidget(self.eigen_rButton)
+
         layout.addWidget(self.enable_sinFFC_checkbox, 0, 0)
-        layout.addWidget(self.eigen_pco_repetitions_label, 1, 0)
-        layout.addWidget(self.eigen_pco_repetitions_entry, 1, 1)
-        layout.addWidget(self.eigen_pco_downsample_label, 2, 0)
-        layout.addWidget(self.eigen_pco_downsample_entry, 2, 1)
-        layout.addWidget(self.downsample_label, 3, 0)
-        layout.addWidget(self.downsample_entry, 3, 1)
+        layout.addItem(rbutton_layout, 1, 0, 1, 2)
+        layout.addWidget(self.eigen_pco_repetitions_label, 2, 0)
+        layout.addWidget(self.eigen_pco_repetitions_entry, 2, 1)
+        layout.addWidget(self.eigen_pco_downsample_label, 3, 0)
+        layout.addWidget(self.eigen_pco_downsample_entry, 3, 1)
+        layout.addWidget(self.downsample_label, 4, 0)
+        layout.addWidget(self.downsample_entry, 4, 1)
 
         self.setLayout(layout)
 
