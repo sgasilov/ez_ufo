66beea7000231cc01e92aa86ef1c2b64fba7fb57
emsliei <iainemslie@gmail.com>
2021-07-30 22:38:32 -0600

    Added setters for nlmdn

--- ez_ufo_qt/GUI/nlmdn.py
+++ ez_ufo_qt/GUI/nlmdn.py
@@ -18,49 +18,66 @@ class NLMDNGroup(QGroupBox):
 
         self.input_dir_button = QPushButton("Select input directory")
         self.input_dir_button.setStyleSheet("background-color:lightgrey; font: 12pt;")
+        self.input_dir_button.clicked.connect(self.set_indir_button)
 
         self.select_img_button = QPushButton("Select one image")
         self.select_img_button.setStyleSheet("background-color:lightgrey; font: 12pt;")
+        self.select_img_button.clicked.connect(self.select_image)
 
         self.input_dir_entry = QLineEdit()
+        self.input_dir_entry.textChanged.connect(self.set_indir_entry)
 
         self.output_dir_button = QPushButton("Select output directory or filename pattern")
         self.output_dir_button.setStyleSheet("background-color:lightgrey; font: 12pt;")
+        self.output_dir_button.clicked.connect(self.set_outdir_button)
 
         self.save_biftiff_checkbox = QCheckBox("Save in bigtiff container")
+        self.save_biftiff_checkbox.clicked.connect(self.set_save_bigtiff)
 
         self.output_dir_entry = QLineEdit()
+        self.output_dir_entry.textChanged.connect(self.set_outdir_entry)
 
         self.similarity_radius_label = QLabel("Radius for similarity search")
         self.similarity_radius_entry = QLineEdit()
+        self.similarity_radius_entry.textChanged.connect(self.set_rad_sim_entry)
 
         self.patch_radius_label = QLabel("Radius of patches")
         self.patch_radius_entry = QLineEdit()
+        self.patch_radius_entry.textChanged.connect(self.set_rad_patch_entry)
 
         self.smoothing_label = QLabel("Smoothing control parameter")
         self.smoothing_entry = QLineEdit()
+        self.smoothing_entry.textChanged.connect(self.set_smoothing_entry)
 
         self.noise_std_label = QLabel("Noise standard deviation")
         self.noise_std_entry = QLineEdit()
+        self.noise_std_entry.textChanged.connect(self.set_noise_entry)
 
         self.window_label = QLabel("Window (optional)")
         self.window_entry = QLineEdit()
+        self.window_entry.textChanged.connect(self.set_window_entry)
 
         self.fast_checkbox = QCheckBox("Fast")
+        self.fast_checkbox.clicked.connect(self.set_fast_checkbox)
 
         self.sigma_checkbox = QCheckBox("Estimate sigma")
+        self.sigma_checkbox.clicked.connect(self.set_sigma_checkbox)
 
         self.help_button = QPushButton("Help")
         self.help_button.setStyleSheet("background-color:lightgrey; font: 13pt; font-weight: bold;")
+        self.help_button.clicked.connect(self.help_button_pressed)
 
         self.delete_button = QPushButton("Delete reco dir")
         self.delete_button.setStyleSheet("background-color:lightgrey; font: 13pt; font-weight: bold;")
+        self.delete_button.clicked.connect(self.delete_button_pressed)
 
         self.dry_button = QPushButton("Dry run")
         self.dry_button.setStyleSheet("background-color:lightgrey; font: 13pt; font-weight: bold;")
+        self.dry_button.clicked.connect(self.dry_button_pressed)
 
         self.apply_button = QPushButton("Apply filter")
         self.apply_button.setStyleSheet("background-color:lightgrey;color:royalblue; font: 13pt; font-weight: bold;")
+        self.apply_button.clicked.connect(self.apply_button_pressed)
 
         self.set_layout()
 
@@ -109,3 +126,58 @@ class NLMDNGroup(QGroupBox):
 
     def set_values_from_params(self):
         pass
+
+    def set_indir_button(self):
+        logging.debug("Select input directory pressed")
+
+    def set_indir_entry(self):
+        logging.debug("Indir entry: " + str(self.input_dir_entry.text()))
+
+    def select_image(self):
+        logging.debug("Select one image button pressed")
+
+    def set_outdir_button(self):
+        logging.debug("Select output directory pressed")
+
+    def set_save_bigtiff(self):
+        logging.debug("Save bigtiff checkbox: " + str(self.save_biftiff_checkbox.isChecked()))
+
+    def set_outdir_entry(self):
+        logging.debug("Outdir entry: " + str(self.output_dir_entry.text()))
+
+    def set_rad_sim_entry(self):
+        logging.debug("Radius for similarity: " + str(self.similarity_radius_entry.text()))
+
+    def set_rad_patch_entry(self):
+        logging.debug("Radius of patches: " + str(self.patch_radius_entry.text()))
+
+    def set_smoothing_entry(self):
+        logging.debug("Smoothing control: " + str(self.smoothing_entry.text()))
+
+    def set_noise_entry(self):
+        logging.debug("Noise std: " + str(self.noise_std_entry.text()))
+
+    def set_window_entry(self):
+        logging.debug("Window: " + str(self.window_entry.text()))
+
+    def set_fast_checkbox(self):
+        logging.debug("Fast: " + str(self.fast_checkbox.isChecked()))
+
+    def set_sigma_checkbox(self):
+        logging.debug("Estimate sigma: " + str(self.sigma_checkbox.isChecked()))
+
+    def quit_button_pressed(self):
+        logging.debug("Quit Button Pressed")
+
+    def help_button_pressed(self):
+        logging.debug("Help Button Pressed")
+
+    def delete_button_pressed(self):
+        logging.debug("Delete Reco Button Pressed")
+
+    def dry_button_pressed(self):
+        logging.debug("Dry Run Button Pressed")
+
+    def apply_button_pressed(self):
+        logging.debug("Apply Filter Button Pressed")
+
